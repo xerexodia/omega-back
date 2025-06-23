4a5e8d8e660b05b626dfb2f3cf2e82740dd2e1e9
xerexodia <ibrahimfarhat2018@gmail.com>
2025-06-23 22:02:17 +0100

    fix build issues

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+(global as any).crypto = crypto;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
