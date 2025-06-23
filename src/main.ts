f515e6f80e9633eafeb573a1dd0bd3f7bd97fae8
xerexodia <ibrahimfarhat2018@gmail.com>
2025-06-23 22:53:33 +0100

    git fix deployements

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import * as crypto from 'crypto';
+import * as crypto from "crypto";
 (global as any).crypto = crypto;
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,13 +14,13 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true,
       },
-    }),
+    })
   );
   app.enableCors({
-    origin: '*', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(3000, "0.0.0.0");
 }
 bootstrap();
